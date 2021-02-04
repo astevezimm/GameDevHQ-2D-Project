7c7b617e45e904333e3fab4346b63c3eb48f098a
Andrew Zimmerman <astevezimm@gmail.com>
2021-02-03 19:33:55 -0700

    Scripting for laser object

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : PooledObject<Projectile>
+{
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float timeOut = 5;
+    
+    private Rigidbody _rigidbody;
+    private float _endTime;
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.velocity = transform.up * speed;
+        _endTime = Time.time + timeOut;
+    }
+
+    private void Update()
+    {
+        if (Time.time  > _endTime)
+            Return();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Return();
+    }
+    
+}
